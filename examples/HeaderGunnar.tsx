5ca690fbc8c734a1c3842a964cb5bc6dfb440914
Celeste Carter <celeste.carter@tcnbroadcasting.com>
2019-06-24 15:54:24 -0600

    gunnars test header

--- examples/HeaderGunnar.tsx
+++ examples/HeaderGunnar.tsx
@@ -6,57 +6,55 @@ import { avengersData } from './avengers-data';
 
 const columns = [
   {
-    title:
-    'url',
-  field:
-  "url",
-  type:
-  'string'
-},
-
+    title: 'url',
+    field: "url",
+    type: 'string'
+  },
   {
-  title:
-  'name/alias',
-field:
-"name/alias",
-type:
-'string'
-},
-
-{
-  title:
-  'appearances',
-field:
-"appearances",
-type:
-'number'
-},
-
-{
-  title:
-  'current?',
-field:
-"current?",
-type:
-'string'
-},
-
-{
-  title:
-  'gender',
-field:
-"gender",
-type:
-'string'
-},
-
-{
-  title:
-  'full/reserve avengers intro', field: "full/reserve avengers intro", type: 'string' },
-  { title: 'year', field: "year", type: 'number' },
-  { title: 'years since joining', field: "years since joining", type: 'number' },
-  { title: 'honorary', field: "honorary", type: 'string' },
-  { title: 'notes', field: "notes", type: 'string' },
+    title: 'name/alias',
+    field: "name/alias",
+    type: 'string'
+  },
+  {
+    title: 'appearances',
+    field: "appearances",
+    type: 'number'
+  },
+  {
+    title: 'current?',
+    field: "current?",
+    type: 'string'
+  },
+  {
+    title: 'gender',
+    field: "gender",
+    type: 'string'
+  },
+  {
+    title: 'full/reserve avengers intro',
+    field: "full/reserve avengers intro",
+    type: 'string'
+  },
+  {
+    title: 'year',
+    field: "year",
+    type: 'number'
+  },
+  {
+    title: 'years since joining',
+    field: "years since joining",
+    type: 'number'
+  },
+  {
+    title: 'honorary',
+    field: "honorary",
+    type: 'string'
+  },
+  {
+    title: 'notes',
+    field: "notes",
+    type: 'string'
+  },
 ];
 
 const HeaderGunnar = () => {
