06e6ac96282215a5d04f88afd3a30894d7740bcf
Jason Clark <jason.clark@tcnbroadcasting.com>
2019-08-21 12:24:27 -0600

    Updated packages and Demo column names

--- examples/Demo.tsx
+++ examples/Demo.tsx
@@ -1,4 +1,4 @@
-import 'material-icons/iconfont/material-icons.css'
+import 'material-icons/iconfont/material-icons.css';
 
 import { Grid } from '@material-ui/core';
 import { MuiThemeProvider } from '@material-ui/core/styles';
@@ -11,8 +11,8 @@ let direction: 'ltr' | 'rtl' = 'ltr';
 const theme = createMuiTheme({
   direction: direction,
   palette: {
-    type: 'light'
-  }
+    type: 'light',
+  },
 });
 
 const bigData: any[] = [];
@@ -27,7 +27,7 @@ for (let i = 0; i < 1; i++) {
     sex: i % 2 ? 'Male' : 'Female',
     type: 'adult',
     insertDateTime: new Date(2018, 1, 1, 12, 23, 44),
-    time: new Date(1900, 1, 1, 14, 23, 35)
+    time: new Date(1900, 1, 1, 14, 23, 35),
   };
   bigData.push(d);
 }
@@ -41,16 +41,88 @@ class Demo extends Component<any, any> {
     text: 'text',
     selecteds: 0,
     data: [
-      { id: 1, name: 'A1', surname: 'B', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 0, sex: 'Male', type: 'adult', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35) },
-      { id: 2, name: 'A2', surname: 'B', isMarried: false, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'adult', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 1 },
-      { id: 3, name: 'A3', surname: 'B', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 1 },
-      { id: 4, name: 'A4', surname: 'C', isMarried: true, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 3 },
-      { id: 5, name: 'A5', surname: 'C', isMarried: false, birthDate: new Date(1987, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35) },
-      { id: 6, name: 'A6', surname: 'C', isMarried: true, birthDate: new Date(1989, 1, 1), birthCity: 34, sex: 'Female', type: 'child', insertDateTime: new Date(2018, 1, 1, 12, 23, 44), time: new Date(1900, 1, 1, 14, 23, 35), parentId: 5 },
+      {
+        id: 1,
+        name: 'A1',
+        surname: 'B',
+        isMarried: true,
+        birthDate: new Date(1987, 1, 1),
+        birthCity: 0,
+        sex: 'Male',
+        type: 'adult',
+        insertDateTime: new Date(2018, 1, 1, 12, 23, 44),
+        time: new Date(1900, 1, 1, 14, 23, 35),
+      },
+      {
+        id: 2,
+        name: 'A2',
+        surname: 'B',
+        isMarried: false,
+        birthDate: new Date(1987, 1, 1),
+        birthCity: 34,
+        sex: 'Female',
+        type: 'adult',
+        insertDateTime: new Date(2018, 1, 1, 12, 23, 44),
+        time: new Date(1900, 1, 1, 14, 23, 35),
+        parentId: 1,
+      },
+      {
+        id: 3,
+        name: 'A3',
+        surname: 'B',
+        isMarried: true,
+        birthDate: new Date(1987, 1, 1),
+        birthCity: 34,
+        sex: 'Female',
+        type: 'child',
+        insertDateTime: new Date(2018, 1, 1, 12, 23, 44),
+        time: new Date(1900, 1, 1, 14, 23, 35),
+        parentId: 1,
+      },
+      {
+        id: 4,
+        name: 'A4',
+        surname: 'C',
+        isMarried: true,
+        birthDate: new Date(1987, 1, 1),
+        birthCity: 34,
+        sex: 'Female',
+        type: 'child',
+        insertDateTime: new Date(2018, 1, 1, 12, 23, 44),
+        time: new Date(1900, 1, 1, 14, 23, 35),
+        parentId: 3,
+      },
+      {
+        id: 5,
+        name: 'A5',
+        surname: 'C',
+        isMarried: false,
+        birthDate: new Date(1987, 1, 1),
+        birthCity: 34,
+        sex: 'Female',
+        type: 'child',
+        insertDateTime: new Date(2018, 1, 1, 12, 23, 44),
+        time: new Date(1900, 1, 1, 14, 23, 35),
+      },
+      {
+        id: 6,
+        name: 'A6',
+        surname: 'C',
+        isMarried: true,
+        birthDate: new Date(1989, 1, 1),
+        birthCity: 34,
+        sex: 'Female',
+        type: 'child',
+        insertDateTime: new Date(2018, 1, 1, 12, 23, 44),
+        time: new Date(1900, 1, 1, 14, 23, 35),
+        parentId: 5,
+      },
     ],
     columns: [
       {
-        title: 'Adı', field: 'name', editComponent: props => {
+        title: 'First Name',
+        field: 'name',
+        editComponent: props => {
           return (
             <input
               value={props.value}
@@ -61,35 +133,48 @@ class Demo extends Component<any, any> {
                 props.onRowDataChange(data);
               }}
             />
-          )
-        }
+          );
+        },
       },
       {
-        title: 'Soyadı', field: 'surname', editComponent: props => {
+        title: 'Last Name',
+        field: 'surname',
+        editComponent: props => {
           this.inputBProps = props;
           return (
-            <input
-              value={props.value}
-              onChange={e => props.onChange(e.target.value)}
-            />
-          )
-        }
+            <input value={props.value} onChange={e => props.onChange(e.target.value)} />
+          );
+        },
       },
-      { title: 'Evli', field: 'isMarried', type: 'boolean' },
-      { title: 'Cinsiyet', field: 'sex', disableClick: true, editable: 'onAdd' },
-      { title: 'Tipi', field: 'type', removable: false, editable: 'never' },
-      { title: 'Doğum Yılı', field: 'birthDate', type: 'date' },
-      { title: 'Doğum Yeri', field: 'birthCity', lookup: { 34: 'İstanbul', 0: 'Şanlıurfa' } },
-      { title: 'Kayıt Tarihi', field: 'insertDateTime', type: 'datetime' },
-      { title: 'Zaman', field: 'time', type: 'time' }
+      { title: 'Married?', field: 'isMarried', type: 'boolean' },
+      { title: 'Gender', field: 'sex', disableClick: true, editable: 'onAdd' },
+      { title: 'Type', field: 'type', removable: false, editable: 'never' },
+      { title: 'Birth Date', field: 'birthDate', type: 'date' },
+      {
+        title: 'Birth City',
+        field: 'birthCity',
+        lookup: { 34: 'İstanbul', 0: 'Şanlıurfa' },
+      },
+      { title: 'Created', field: 'insertDateTime', type: 'datetime' },
+      { title: 'Time', field: 'time', type: 'time' },
     ],
     remoteColumns: [
-      { title: 'Avatar', field: 'avatar', render: rowData => <img style={{ height: 36, borderRadius: '50%' }} src={rowData.avatar} /> },
+      {
+        title: 'Avatar',
+        field: 'avatar',
+        render: rowData => (
+          <img
+            style={{ height: 36, borderRadius: '50%' }}
+            src={rowData.avatar}
+            alt="Avatar"
+          />
+        ),
+      },
       { title: 'Id', field: 'id' },
       { title: 'First Name', field: 'first_name', defaultFilter: 'De' },
       { title: 'Last Name', field: 'last_name' },
-    ]
-  }
+    ],
+  };
 
   render() {
     return (
@@ -110,7 +195,10 @@ class Demo extends Component<any, any> {
               </Grid>
             </Grid>
             {this.state.text}
-            <button onClick={() => this.tableRef.current.onAllSelected(true)} style={{ margin: 10 }}>
+            <button
+              onClick={() => this.tableRef.current.onAllSelected(true)}
+              style={{ margin: 10 }}
+            >
               Select
             </button>
             <MaterialTable
@@ -133,25 +221,26 @@ class Demo extends Component<any, any> {
               ]}
               options={{
                 grouping: true,
-                filtering: true
+                filtering: true,
               }}
-              data={query => new Promise((resolve) => {
-                let url = 'https://reqres.in/api/users?'
-                url += 'per_page=' + query.pageSize
-                url += '&page=' + (query.page + 1)
-                console.log(query);
-                fetch(url)
-                  .then(response => response.json())
-                  .then(result => {
-                    resolve({
-                      data: result.data,
-                      page: result.page - 1,
-                      totalCount: result.total,
-                    })
-                  })
-              })}
+              data={query =>
+                new Promise(resolve => {
+                  let url = 'https://reqres.in/api/users?';
+                  url += 'per_page=' + query.pageSize;
+                  url += '&page=' + (query.page + 1);
+                  console.log(query);
+                  fetch(url)
+                    .then(response => response.json())
+                    .then(result => {
+                      resolve({
+                        data: result.data,
+                        page: result.page - 1,
+                        totalCount: result.total,
+                      });
+                    });
+                })
+              }
             />
-
           </div>
         </MuiThemeProvider>
       </>
